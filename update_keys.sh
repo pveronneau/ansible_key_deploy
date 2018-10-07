40a43db3ece42f4b4f085ab42358644e72e7d17e
Patrick Veronneau <patrick@decepticons.ca>
2018-10-07 14:26:07 -0600

    updated update script! HA!

--- update_keys.sh
+++ update_keys.sh
@@ -2,18 +2,35 @@
 KEYBASE_USER=pveronneau
 KEYBASE_PROXY=socks5://localhost:10700
 KEYBASE_KEYNAME=id_rsa.pub
+CHECKSUM="4f641ad19e710860ac12d90bf3e13833729d93f77641807e06c7ad9cab82f31ba2e193da8169ab82d09e2a4720e4afa406a78453f1adc33dd89463963e2a348f authorized_keys"
 
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
 if curl -s --fail -m 5 -X GET https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > /dev/null; then
-	curl -s https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > ~/.ssh/authorized_keys && echo -e "\e[92mupdated keys\e[0m" && exit 0 || echo -e "\e[31mfailed to update keys\e[0m"
+	curl -s https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > ~/.ssh/authorized_keys && echo -e "\e[92mupdated keys\e[0m" || echo -e "\e[31mfailed to update keys\e[0m"
+	echo "Verifying the expected sha512 checksum on ~/.ssh/authorized_keys"
+	if cd ~/.ssh/ && echo $CHECKSUM | sha512sum --quiet -c -; then
+		echo -e "\e[92mChecksum valid\e[0m"
+	else
+		echo -e "\e[31mChecksum $CHECKSUM is invalid, purging the entry\e[0m"
+		rm ~/.ssh/authorized_keys
+	fi;
+	exit 0
 else
     echo -e "\e[33mfailed to connect directly, adding proxy\e[0m"
     export https_proxy=$KEYBASE_PROXY
     if curl -s --fail  -m 5 -X GET https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > /dev/null; then
-	    curl -s https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > ~/.ssh/authorized_keys && echo -e "\e[92mupdated keys with proxy\e[0m" && exit 0 || echo -e "\e[31mfailed to update keys with proxy\e[0m"
+	    curl -s https://$KEYBASE_USER.keybase.pub/$KEYBASE_KEYNAME > ~/.ssh/authorized_keys && echo -e "\e[92mupdated keys with proxy\e[0m" || echo -e "\e[31mfailed to update keys with proxy\e[0m"
 	    unset https_proxy
+		echo "Verifying the expected sha512 checksum on ~/.ssh/authorized_keys"
+		if cd ~/.ssh/ && echo $CHECKSUM | sha512sum --quiet -c -; then
+			echo -e "\e[92mChecksum valid\e[0m"
+		else
+			echo -e "\e[31mChecksum $CHECKSUM is invalid, purging the entry\e[0m"
+			rm ~/.ssh/authorized_keys
+		fi;
+		exit 0
 	else
 	    echo -e "\e[31mfailed to connect with proxy\e[0m"
 	    unset https_proxy
@@ -32,4 +49,13 @@ else
 	    echo -e "\e[31mfailed to connect with proxy insecurely\e[0m"
 	    unset https_proxy
 	fi;
+fi;
+
+#Verify the file
+echo "Verifying the expected sha512 checksum on ~/.ssh/authorized_keys"
+if cd ~/.ssh/ && echo $CHECKSUM | sha512sum --quiet -c -; then
+	echo -e "\e[92mChecksum Valid\e[0m"
+else
+	echo -e "\e[31mChecksum $CHECKSUM is invalid, purging the entry\e[0m"
+	rm ~/.ssh/authorized_keys
 fi;
